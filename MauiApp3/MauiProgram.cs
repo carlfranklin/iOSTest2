e5afb1dbd5e6e12740e201bd141267a30ce496f9
Carl Franklin <carl@appvnext.com>
2023-05-01 19:37:58 -0400

    Add project files.

---
+++ MauiApp3/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿global using Plugin.Maui.Audio;
+using Microsoft.Extensions.Logging;
+using MauiApp3.Data;
+
+namespace MauiApp3;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddSingleton(AudioManager.Current);
+        return builder.Build();
+	}
+}
